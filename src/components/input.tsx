65faf53dce69980186154f1c5ab1846c3b2df697
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-03-28 18:58:08 -0300

    FEAT commitao

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,11 +1,23 @@
 import {PlusCircle} from 'phosphor-react'
+import { FormEvent, useState } from 'react'
 
 import styles from './input.module.css'
 
 export function Input () {
+  const [myTasks, setMyTasks] = useState([
+    'hello',
+  ])
+
+  function handleNewTask (event: FormEvent) {
+    event.preventDefault()
+
+    setMyTasks([...myTasks, event.target.newTask.value ])
+    console.log(myTasks)
+  }
+
   return (
-    <form className={styles.form}>
-      <textarea placeholder="Adicione uma nova tarefa"></textarea>
+    <form className={styles.form} onSubmit={handleNewTask}>
+      <textarea name='newTask' placeholder="Adicione uma nova tarefa"></textarea>
       <button type='submit'>Criar <PlusCircle size={20}/></button>
     </form>
   )
