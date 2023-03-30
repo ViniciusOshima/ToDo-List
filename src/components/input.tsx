270c0d7438920d4d8cfa111a21c40d0e4a6ef3b9
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-03-30 16:34:21 -0300

    FEAT validação do botão input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,20 +1,22 @@
-import {PlusCircle} from 'phosphor-react'
-import { ChangeEvent, FormEvent, useState } from 'react'
+import { PlusCircle } from 'phosphor-react'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 
 import styles from './input.module.css'
 
 interface AddTasksProps {
-  addTasks: (myTasks:string) => void
+  addTasks: (myTasks: string) => void
 }
 
-export function Input ({addTasks}:AddTasksProps) {
+export function Input({ addTasks }: AddTasksProps) {
   const [taskChange, setTaskChange] = useState('')
 
-  function handleNewTaskChange (event: ChangeEvent<HTMLTextAreaElement>) {
+  function handleNewTaskChange(event: ChangeEvent<HTMLTextAreaElement>) {
     setTaskChange(event.target.value)
+
+    event.target.setCustomValidity('')
   }
 
-  function handleNewTask (event: FormEvent) {
+  function handleNewTask(event: FormEvent) {
     event.preventDefault()
 
     addTasks(taskChange)
@@ -22,10 +24,23 @@ export function Input ({addTasks}:AddTasksProps) {
     setTaskChange('')
   }
 
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
+  }
+
   return (
     <form className={styles.form} onSubmit={handleNewTask}>
-      <textarea value={taskChange} onChange={handleNewTaskChange} name='newTask' placeholder="Adicione uma nova tarefa"></textarea>
-      <button type='submit'>Criar <PlusCircle size={20}/></button>
+      <textarea
+        onInvalid={handleNewTaskInvalid}
+        required
+        value={taskChange}
+        onChange={handleNewTaskChange}
+        name="newTask"
+        placeholder="Adicione uma nova tarefa"
+      ></textarea>
+      <button type="submit" disabled={taskChange.length == 0}>
+        Criar <PlusCircle size={20} />
+      </button>
     </form>
   )
 }
